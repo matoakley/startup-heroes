e5d278997f2c8e9e7e7b4feadb1945c1de25bfb9
Matt Oakley <matt.oakley@hashbanghq.com>
2018-01-01 15:35:11 +0000

    add many new landing pages

--- Gemfile
+++ Gemfile
@@ -4,3 +4,15 @@ gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
+
+gem 'rake', '~> 12.3'
+
+gem "middleman-sprockets"
+
+gem 'font-awesome-sass'
+
+gem 'middleman-seo_sitemap', path: '~/Projects/startup-heroes/middleman/middleman-seo_sitemap'
+
+gem 'middleman-robots'
+
+gem "middleman-imageoptim", :git => "https://github.com/plasticine/middleman-imageoptim", :branch => "master"
--- config.rb
+++ config.rb
@@ -1,9 +1,21 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+#activate :imageoptim
+
+activate :robots, sitemap: 'https://www.startupheroes.co.uk/sitemap.xml'
+
+activate :seo_sitemap do |sitemap|
+  sitemap.default_host = 'https://www.startupheroes.co.uk'
+end
+
+activate :directory_indexes
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
+
+activate :sprockets
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
@@ -40,7 +52,7 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
